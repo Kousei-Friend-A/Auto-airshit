2a83ba4c90d645adeb51155a4ef4f2a885780685
N3TH1K4 <88619598+N3TH1K4@users.noreply.github.com>
2022-10-04 17:20:35 +0530

    Add files via upload

---
+++ main/inline.py
@@ -0,0 +1,21 @@
+from config import INDEX_USERNAME, SCHEDULE_ID, STATUS_ID, UPLOADS_USERNAME
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+button1 = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(text="View Schedule", url= f"https://t.me/{UPLOADS_USERNAME}/{SCHEDULE_ID}")
+            ],
+            [
+                InlineKeyboardButton(text="Index Channel", url= f"https://t.me/{INDEX_USERNAME}"),
+            ]
+        ]
+    )
+
+button2 = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(text="Check Queue", url= f"https://t.me/{UPLOADS_USERNAME}/{STATUS_ID}")
+            ]
+        ]
+    )
